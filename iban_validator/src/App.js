d42772d4d39823941daa05a8d13de4dfd1f32db2
vuralbatuhan <vuralbatuhan01@gmail.com>
2023-04-07 11:32:02 +0300

    // #Please enter the commit message for your changes. Lines starting // # with '#' will be ignored, and an empty message aborts the commit. // # // # On branch master // # Your branch is up to date with 'iban-validator/master'. // # // # Changes to be committed: // #	modified:   iban_validator/src/App.js

--- iban_validator/src/App.js
+++ iban_validator/src/App.js
@@ -2,7 +2,11 @@ import 'bv-proje';
 
 const IBANValidator = require('bv-proje');
 const message = IBANValidator.isValid('TR330006 1005 1978 6457 8413 26');
-console.log(message)
+console.log(message); // true
+
+const message1 =IBANValidator.isValid('TR330006 1005 1978 6457 8');
+console.log(message1); //false
+
 
 function App() {
   return (
