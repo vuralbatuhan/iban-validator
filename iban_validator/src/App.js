4ea342cde430f84f7a37855ab037efe22335990b
vuralbatuhan <vuralbatuhan01@gmail.com>
2023-04-07 11:08:41 +0300

    iban-validator

---
+++ iban_validator/src/App.js
@@ -0,0 +1,14 @@
+import 'bv-proje';
+
+const IBANValidator = require('bv-proje');
+const message = IBANValidator.isValid('TR330006 1005 1978 6457 8413 26');
+console.log(message)
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
